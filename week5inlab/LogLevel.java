da71d1cd4ef11bb01bd79a8c6e5921bafb0bbbf7
Abhijith32016 <2320030006@klh.edu.in>
2024-09-14 11:34:24 +0530

    Delete week5inlab directory

--- week5inlab/LogLevel.java
+++
@@ -1,7 +0,0 @@
-package week5inlab;
-
-public class LogLevel {
-    public static final String INFO = "INFO";
-    public static final String DEBUG = "DEBUG";
-    public static final String ERROR = "ERROR";
-}
